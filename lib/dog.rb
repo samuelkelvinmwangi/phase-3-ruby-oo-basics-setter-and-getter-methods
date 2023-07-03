8cc52d17c720e45702ec34667ccc565bc25f3ebe
samuel <samuelkelvinmwangi@gmail.com>
2023-07-03 22:32:19 +0300

    finishing setter and getter assingnment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,37 @@
 class Dog
+    def name = (name)
+        @name = name
+    end
+    def name
+        @name
+    end
+
+    def breed = (breed)
+        @breed = breed
+    end
+    def breed
+        @breed
+    end
 
 end
+
+breed = Dog.new
+breed.name = "breed"
+
+class Person
+    def name = (name)
+        @name = name
+    end
+    def name
+        @name
+    end
+
+    def job = (job)
+        @job = job
+    end
+    def job
+        @job
+    end
+end
+job = Person.new
+job.name = "job"
